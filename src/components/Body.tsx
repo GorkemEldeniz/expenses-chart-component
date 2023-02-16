6eed72a86e3cbf70d738331780ffd4ac42f7d65d
GorkemEldeniz <gorkemeldeniz30@gmail.com>
2023-02-16 16:06:34 +0300

    fixed max amount and hover statements

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import Graph from "./Graph";
 import * as S from "../styled/index";
+import data from "../../data.json";
+import { Idata } from './Graph';
 
 function Body() {
+
+  let totalAmount = data.reduce((acc: number, cur: Idata) => acc + cur.amount, 0);
+
   return (
     <S.Body>
       <S.H1>Spending - Last 7 days</S.H1>
@@ -13,7 +18,7 @@ function Body() {
       <div>
         <S.Span>Total this month</S.Span>
         <S.Footer>
-          <S.H1>$478.33</S.H1>
+          <S.H1>${totalAmount}</S.H1>
           <div>
             <div>+2.4%</div>
             <S.Span>from last month</S.Span>
--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -3,16 +3,27 @@ import * as S from "../styled/index";
 import data from "../../data.json";
 import { useState } from 'react';
 
+export type Idata = {
+  day: string;
+  amount: number;
+}
+
 function Graph() {
   const [hovered, setHovered] = useState<any | number>(null);
+  let shallowData = [...data];
+  const maxAmount = shallowData.sort((a: Idata, b: Idata) => b?.amount - a?.amount)[0]?.amount;
 
+
+  const handleLeave = (e: React.MouseEvent<HTMLDivElement>) => {
+    setHovered(null);
+  }
   return (
     <S.Graph>
       {data.map(({ day, amount }, index) => <div className='col' key={index}>
-        <S.Column onMouseOver={() => setHovered(index)} active={index == 3} amount={amount * 1.5}>
+        <S.Column onMouseOver={() => setHovered(index)} onMouseOut={handleLeave} active={amount === maxAmount} amount={amount * 1.5}>
           {hovered == index && <S.PopUp>${amount}</S.PopUp>}
         </S.Column>
-        <S.Span onMouseOver={() => setHovered(index)} >{day}</S.Span>
+        <S.Span>{day}</S.Span>
       </div>)}
     </S.Graph>
   )
