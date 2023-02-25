ab26f78cf8ae2291e20373c19175b7aec90723c0
GorkemEldeniz <gorkemeldeniz30@gmail.com>
2023-02-25 11:52:14 +0300

    maxAmount tan currentDay e geçildi

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -11,8 +11,11 @@ export type Idata = {
 function Graph() {
   const [hovered, setHovered] = useState<any | number>(null);
   let shallowData = [...data];
-  const maxAmount = shallowData.sort((a: Idata, b: Idata) => b?.amount - a?.amount)[0]?.amount;
+  //const maxAmount = shallowData.sort((a: Idata, b: Idata) => b?.amount - a?.amount)[0]?.amount;
 
+  const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+  let currentDay = daysOfWeek[new Date().getDay() - 1];
 
   const handleLeave = (e: React.MouseEvent<HTMLDivElement>) => {
     setHovered(null);
@@ -20,7 +23,7 @@ function Graph() {
   return (
     <S.Graph>
       {data.map(({ day, amount }, index) => <div className='col' key={index}>
-        <S.Column onMouseOver={() => setHovered(index)} onMouseOut={handleLeave} active={amount === maxAmount} amount={amount * 1.5}>
+        <S.Column onMouseOver={() => setHovered(index)} onMouseOut={handleLeave} active={day === currentDay} amount={amount * 1.5}>
           {hovered == index && <S.PopUp>${amount}</S.PopUp>}
         </S.Column>
         <S.Span>{day}</S.Span>
